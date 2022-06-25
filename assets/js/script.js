45679841e593bb1ec5e275942a8e6b05fe4a8cbb
unknown <primaaimede@gmail.com>
2022-06-25 10:47:19 +0700

    first comit

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+$(".nav-link").click(function (e) {
+  $(".navbar-toggler").click();
+});
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 25) {
+    $(".navbar").css("paddingTop", "9px");
+    $(".navbar-brand").css("color", "white");
+    $(".nav-link").css("color", "white");
+    $(".bg-blueviolet").css("backgroundColor", "#031e41");
+    $(".navbar-brand").hover(
+      function () {
+        $(this).css("color", "rgba(255, 255, 255, 0.75)");
+      },
+      function () {
+        $(this).css("color", "white");
+      }
+    );
+    $(".nav-link").hover(
+      function () {
+        $(this).css("color", "rgba(255, 255, 255, 0.75)");
+      },
+      function () {
+        $(this).css("color", "white");
+      }
+    );
+
+    if ($(this).width() > 980) {
+      $(".navbar-toggler").css("display", "none");
+    } else {
+      $(".navbar-toggler").css("display", "block");
+    }
+  } else if ($(this).scrollTop() == 0) {
+    $(".navbar").css("paddingTop", "40px");
+    $(".navbar-brand").css("color", "#031e41");
+    $(".nav-link").css("color", "#031e41");
+    $(".bg-blueviolet").css("backgroundColor", "");
+    $(".navbar-toggler").css("display", "none");
+    $(".navbar-brand").hover(
+      function () {
+        $(this).css("color", "rgba(255, 255, 255, 0.75)");
+      },
+      function () {
+        $(this).css("color", "#031e41");
+      }
+    );
+    $(".nav-link").hover(
+      function () {
+        $(this).css("color", "rgba(255, 255, 255, 0.75)");
+      },
+      function () {
+        $(this).css("color", "#031e41");
+      }
+    );
+  }
+});
